327c1ca43fce482a055cd3d696e9c52b2fb44e7e
Vinicius Veras <andvinicius1@gmail.com>
2018-10-04 20:24:10 -0300

    GUI v.0.2

--- SistemaFac/MainWindow.xaml.cs
+++ SistemaFac/MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using MahApps.Metro.Controls.Dialogs;
 using System.Diagnostics;
 using MahApps.Metro.IconPacks;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace SistemaFac
 {
@@ -18,16 +22,49 @@ namespace SistemaFac
     {
         public MainWindow()
         {
- 
+
 
             InitializeComponent();
-            dpTipoArquivo.ItemsSource = Enum.GetValues(typeof(TipoArquivo)).Cast<TipoArquivo>();
 
 
-            //Leitor.Ler.Arquivo()
-      
+            Msgm();
+            var dataTable = new DataTable();
 
-            //DataContext = new TesteMVVM();
+            using (var conn = new SQLiteConnection(@"Data Source = C:\Users\andvi\Desktop\EMPRES.db;"))
+            {
+                conn.Open();
+                
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    
+                    Debug.WriteLine("abriu");
+
+                    using (var comm = new SQLiteCommand(conn))
+                    {
+                        comm.CommandText = "SELECT IdEmpresa, Nome FROM EMPRESA";
+
+                        Console.WriteLine("DataAdapter:");
+                        var adapter = new SQLiteDataAdapter(comm);
+
+                        adapter.Fill(dataTable);
+                        //foreach (DataRow row in dataTable.Rows)
+                        //{
+                        //    Console.WriteLine("Nome do Cliente: {0}", row["Nome"]);
+                        //}
+                    }
+                }
+
+                //    //cboxExibicaoEmpresa.DisplayMemberPath = "nome";
+                //    //cboxExibicaoEmpresa
+
+                cboxExibicaoEmpresa.DataContext = dataTable.DefaultView;
+                dpTipoArquivo.ItemsSource = Enum.GetValues(typeof(TipoArquivo)).Cast<TipoArquivo>();
+
+
+                //Leitor.Ler.Arquivo()
+
+                //DataContext = new TesteMVVM();
+            }
         }
 
         private void dpTipoArquivo_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -157,22 +194,93 @@ namespace SistemaFac
             Msgm();
         }
 
+
+
         private async void Msgm()
         {
-           var CONTROLLER = await this.ShowMessageAsync("teste", "Processando", MessageDialogStyle.Affirmative, new MetroDialogSettings
-           {
-               AnimateHide = false
-           });
+            LoginDialogData result = await this.ShowLoginAsync("Autenticação", "Entre com suas Credenciais", new LoginDialogSettings { ColorScheme = this.MetroDialogOptions.ColorScheme, UsernameWatermark = "Login" });
 
-            var controll = await this.ShowProgressAsync("Processando", "Aguarde ...", true);
 
-            for (int i = 0; i < 100; i++)
+            while (result == null|| !(result.Username.Equals("Teste")))
             {
-                await Task.Delay(2000);
-                controll.SetProgress(.25);
+                await this.ShowMessageAsync("Erro de Autenticação","Usuario ou Senha Invalidos");
+                result = await this.ShowLoginAsync("Autenticação", "Entre com suas credenciais", new LoginDialogSettings { ColorScheme = this.MetroDialogOptions.ColorScheme, UsernameWatermark = "Login" });
+            }
+
+
+            if(result == null)
+            {
 
             }
-            await controll.CloseAsync();
-        } 
+
+
+            //var CONTROLLER = await this.ShowMessageAsync("teste", "Processando", MessageDialogStyle.Affirmative, new MetroDialogSettings
+            //{
+            //    AnimateHide = false
+            //});
+
+            // var controll = await this.ShowProgressAsync("Processando", "Aguarde ...", true);
+
+            // for (int i = 0; i < 100; i++)
+            // {
+            //     await Task.Delay(2000);
+            //     controll.SetProgress(.25);
+
+            // }
+            // await controll.CloseAsync();
+        }
+
+        //private void ToggleSwitch_Click(object sender, RoutedEventArgs e)
+        //{
+        //    txboxExibicaoDr.Text = "72";
+        //    txboxExibicaoNumContrato.Text = "111111";
+        //    txboxExibicaoCepOrigem.Text = "01000-000";
+        //    txboxExibicaoCepDestino.Text = "05000-000";
+        //    txboxExibicaoCodSto.Text = "0999999";
+        //}
     }
+
+
+
+    #region Teste de Validaçoes
+    public class Contrato : IDataErrorInfo
+    {
+        private string _numContrato;
+
+        public string NumContrato
+        {
+            get
+            {
+                return this._numContrato;
+            }
+            set
+            {
+                _numContrato = value;
+            }
+        }
+
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string message = string.Empty;
+
+                if (columnName.Equals("NumContrato") &&  _numContrato != null)
+                {
+                    if (Regex.IsMatch(_numContrato, @"^\d{8}\b"))
+                         message = string.Empty;
+                    else
+                        message =  "Campo deve conter apenas 8 dígitos";
+                }
+
+                return message;
+            }
+        }
+
+        public string Error => null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    #endregion
 }
